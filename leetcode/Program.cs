4d4a96bf5afd6f6ee676a652a731ac771e9e7af4
Agenicy <agenicy@gmail.com>
2022-06-21 16:40:43 +0800

    1642 WA

--- leetcode/Program.cs
+++ leetcode/Program.cs
@@ -1,10 +1,12 @@
-﻿#define Q820
+﻿#define Q1642
 
 using System;
 using System.Collections.Generic;
 
 #if Q820
 using Q820;
+#elif Q1642
+using Q1642;
 #endif
 
 namespace leetcode
@@ -16,6 +18,21 @@ namespace leetcode
 			Solution solution = new Solution();
 #if Q820
 			Console.WriteLine(solution.MinimumLengthEncoding(new[] { "time", "me", "bell" }));
+#elif Q1642
+			/*Console.WriteLine(solution.FurthestBuilding(new[] { 4, 2, 7, 6, 9, 14, 12 }, 5, 1));
+			Console.WriteLine(solution.FurthestBuilding(new[] { 4, 12, 2, 7, 3, 18, 20, 3, 19 }, 10, 2));
+			Console.WriteLine(solution.FurthestBuilding(new[] { 14, 3, 19, 3 }, 17, 0));
+			Console.WriteLine("The answer should be 4/ 7/ 3");*/
+
+			string[] lines = System.IO.File.ReadAllLines("../../../testcase/1642/testcase1.txt");
+			List<int> vs = new List<int>();
+			foreach (var item in lines[0].Split(','))
+			{
+				vs.Add(int.Parse(item));
+			}
+			Console.WriteLine(solution.FurthestBuilding(vs.ToArray(), int.Parse(lines[1]), int.Parse(lines[2])));
+			Console.WriteLine("The answer should be 72329");
+
 #endif
 		}
 	}
---
+++ leetcode/source/1642.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Q1642
+{
+	public class Solution
+	{
+		class Gap
+		{
+			public int index, value;
+			public Gap(int index, int value)
+			{
+				this.index = index;
+				this.value = value;
+				int_IndexRecord.Add(index);
+			}
+
+			public static List<int> int_IndexRecord = new List<int>();
+		}
+
+		public int FurthestBuilding(int[] heights, int bricks, int ladders)
+		{
+			List<Gap> allGap = new List<Gap>();
+
+			for (int i = 0; i < heights.Length - 1; i++)
+			{
+				int gap = heights[i + 1] - heights[i];
+				if (gap > 0)
+					allGap.Add(new Gap(i, gap));
+			}
+
+			List<Gap> sortestGap = new List<Gap>(allGap);
+			sortestGap.Sort((a, b) => b.value.CompareTo(a.value));
+
+			int farthest = heights.Length - 1;
+
+			long sum = Check(sortestGap, ladders);
+			while (sum > bricks)
+			{
+				/* solve by greedy
+				 * see a ladder as some bricks,
+				 * assume Gap A = 10 at position 1; Gap B = 8 at position 2; Gap C = 9 at position 3,
+				 * put a ladder at A can save more bricks.
+				 * 
+				 * So, no matter how far we want to travel, put a ladder at A is the best strategy.
+				 * 
+				 * Another example:
+				 * assume Gap A = 9 at position 1; Gap B = 8 at position 2; Gap C = 10 at position 3
+				 * put a ladder at C is the best strategy.
+				 * but if the sum of brick is only 16, which can't reach position 3
+				 * we can trim the last position (which means 3) and try again.
+				 * This way, we simplified the question to: Gap A = 9 at position 1; Gap B = 8 at position 2
+				 * thus, put a ladder at A is still the best strategy.
+				 */
+				/* Finally, we got a strategy that:
+				 * swap the biggest gap as a ladder, if we still can't pass,
+				 * trim the last building and try it again.
+				 */
+				int rear = Gap.int_IndexRecord.Count - 1;
+				farthest = Gap.int_IndexRecord[rear];
+
+				sum -= allGap[rear].value;
+				allGap.RemoveAt(rear);
+				sortestGap.RemoveAll(x => x.index == farthest);
+				Gap.int_IndexRecord.RemoveAt(rear);
+
+				allGap.Sort((a, b) => b.value.CompareTo(a.value));
+
+			}
+			return farthest;
+		}
+
+		long Check(List<Gap> sortedGap, int from)
+		{
+			long sum = 0;
+			checked
+			{
+				for (int i = from; i < sortedGap.Count; i++)
+				{
+					sum += sortedGap[i].value;
+				}
+			}
+			return sum;
+		}
+	}
+}
