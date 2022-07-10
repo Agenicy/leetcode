709f4059ccad8a6c38e0c78042e8f8412bd80554
Agenicy <agenicy@gmail.com>
2022-07-10 17:10:14 +0800

    5 AC (27/5)

--- leetcode/source/5.cs
+++ leetcode/source/5.cs
@@ -10,55 +10,77 @@ namespace leetcode.Q5
 		{
 			Solution solution = new Solution();
 
-			Console.WriteLine(solution.LongestPalindrome("xxcxcabacabac"));
-			Console.WriteLine("The answer should be  cabacabac");
+			Console.WriteLine(solution.LongestPalindrome("aacabdkacaa"));
+			Console.WriteLine("The answer should be aca");
 			Console.WriteLine(solution.LongestPalindrome("babad"));
 			Console.WriteLine("The answer should be bab/ aba");
 			Console.WriteLine(solution.LongestPalindrome("cbbd"));
 			Console.WriteLine("The answer should be bb");
+			Console.WriteLine(solution.LongestPalindrome("xxcxcabacabac"));
+			Console.WriteLine("The answer should be  cabacabac");
+		}
+
+		public static void Print(int[,] arr)
+		{
+			var rowCount = arr.GetLength(0);
+			var colCount = arr.GetLength(1);
+			for (int row = 0; row < rowCount; row++)
+			{
+				for (int col = 0; col < colCount; col++)
+					Console.Write(String.Format("{0} ", arr[row, col]));
+				Console.WriteLine();
+			}
+			Console.WriteLine();
 		}
 	}
 	public class Solution
 	{
-		string GetRow(int[,] matrix, int row)
-		{
-			var colLength = matrix.GetLength(0);
-			var ret = "";
-
-			for (var i = 0; i < colLength; i++)
-				ret += $"{matrix[row, i]},";
-
-			return ret;
-		}
-
 		public string LongestPalindrome(string s)
 		{
-			/* This prob equals to : the Longest Common Substring between s and reverse s.
-			 * By https://www.geeksforgeeks.org/longest-common-substring-dp-29/
-			 * the Longest Common Substring can solved by dp
-			 * LCSuff(X, Y, m, n) = LCSuff(X, Y, m-1, n-1) + 1 if X[m-1] = Y[n-1] 
-			 * So, LCSuff(X, X, m, n) = LCSuff(X, X, m-1, n+1) + 1 if X[m-1] =X[n+1] 
+			if (s.Length == 1 || (s.Length == 2 && s[0] == s[1]))
+				return s;
+			/* 
+			 * R(s, m, n) = 0 if R(s, m+1, n-1) == 0 || s[m+1] != s[n-1]
+			 * R(s, m, n) = R(s, m+1, n-1) + 1 if s[m+1] == s[n-1]
 			 */
-			int maxR = 0, max = 0;
-			int[,] dp = new int[s.Length+1, s.Length+1];
-			for (int i = 1; i < s.Length+1; i++)
+			int maxR = 0, max = 1;
+			int[,] dp = new int[s.Length, s.Length];
+			for (int i = 0; i < s.Length; i++)
 			{
-				for (int j = 1; j < s.Length+1; j++)
+				for (int j = 1; j <= 2 && i + j < s.Length; j++)
 				{
-					if (s[i - 1] == s[s.Length - j])
+					if (s[i] == s[i + j])
+						dp[i, i + j] = j + 1;
+					if (dp[i, i + j] > max)
 					{
-						dp[i, j] = dp[i - 1, j - 1] + 1;
+						max = dp[i, i + j];
+						maxR = i;
+					}
+				}
+			}
+			for (int i = s.Length - 1; i >= 0; i--)
+			{
+				for (int j = i + 2; j < s.Length; j++)
+				{
+					if (dp[i, j] != 0)
+						continue;
+
+					if (s[i] != s[j])
+						dp[i, j] = 0;
+					else if (dp[i + 1, j - 1] == 0)
+						dp[i, j] = 0;
+					else
+					{
+						dp[i, j] = dp[i + 1, j - 1] + 2;
 						if (dp[i, j] > max)
 						{
 							max = dp[i, j];
 							maxR = i;
 						}
 					}
-					else
-						dp[i, j] = 0;
 				}
 			}
-			return s.Substring(maxR - max, max);
+			return s.Substring(maxR, max);
 		}
 	}
 }
