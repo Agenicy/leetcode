9666b49b6a826b790843d38f1b1d8ea50e00d168
Agenicy <agenicy@gmail.com>
2022-07-27 14:36:30 +0800

    114 good AC (75/25)

---
+++ leetcode/source/114.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace leetcode.Q114
+{
+	public class Program
+	{
+		public static void Run()
+		{
+			Solution solution = new Solution();
+
+			Log.Print(solution.ToString());
+			Log.Print("The answer should be ");
+		}
+	}
+	public class Solution
+	{
+		public void Flatten(TreeNode root)
+		{
+			if (root == null) return;
+
+			TreeNode treeNode = root;
+			TreeNode last = null;
+			void DFS(TreeNode now)
+			{
+				if (now == null) return;
+
+				TreeNode left = now.left;
+				TreeNode right = now.right;
+
+				now.left = null;
+				if(last != null)
+					last.right = now;
+				last = now;
+
+				DFS(left);
+				DFS(right);
+			}
+			DFS(root);
+		}
+	}
+}
