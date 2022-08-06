c329379cdf8ff0ff6a23a83e765181d0beb0926f
Agenicy <agenicy@gmail.com>
2022-08-06 16:20:42 +0800

    458 AC but Idn why (80/20)

--- leetcode/Program.cs
+++ leetcode/Program.cs
@@ -8,7 +8,7 @@ namespace leetcode
 	{
 		static void Main(string[] args)
 		{
-			Q377.Program.Run();
+			Q458.Program.Run();
 		}
 	}
 	#region fold
---
+++ leetcode/source/458.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace leetcode.Q458
+{
+	public class Program
+	{
+		public static void Run()
+		{
+			Solution solution = new Solution();
+
+			Log.Print(solution.PoorPigs(1000, 15, 60));
+			Log.Print(solution.PoorPigs(4, 15, 15));
+			Log.Print(solution.PoorPigs(4, 15, 30));
+			Log.Print("The answer should be 5/ 2/ 2");
+		}
+	}
+	public class Solution
+	{
+		public int PoorPigs(int buckets, int minutesToDie, int minutesToTest)
+		{
+			int T = minutesToTest/minutesToDie;
+
+			for (int i = 0; i < buckets; i++)
+			{
+				if (Math.Pow(T + 1, i) >= buckets) 
+					return i;
+			}
+			return buckets;
+		}
+	}
+}
