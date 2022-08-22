781b40b0c40b747941f0c311b4e9e2bb0c5a2e65
Agenicy <agenicy@gmail.com>
2022-08-22 20:47:27 +0800

    342 AC(67/66)

--- leetcode/Program.cs
+++ leetcode/Program.cs
@@ -8,7 +8,7 @@ namespace leetcode
 	{
 		static void Main(string[] args)
 		{
-			Q936.Program.Run();
+			Q342.Program.Run();
 		}
 	}
 	#region fold
---
+++ leetcode/source/342.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace leetcode.Q342
+{
+	public class Program
+	{
+		public static void Run()
+		{
+			Solution solution = new Solution();
+
+			Log.Print(solution.ToString());
+			Log.Print("The answer should be ");
+		}
+	}
+	public class Solution
+	{
+
+		public bool IsPowerOfFour(int n)
+		{
+			switch (n)
+			{
+				case 1:
+				case 4:
+				case 16:
+				case 64:
+				case 256:
+				case 1024:
+				case 4096:
+				case 16384:
+				case 65536:
+				case 262144:
+				case 1048576:
+				case 4194304:
+				case 16777216:
+				case 67108864:
+				case 268435456:
+				case 1073741824:
+					return true;
+				default:
+					return false;
+			}
+		}
+	}
+}
