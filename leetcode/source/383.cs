049e10316243999a3f47596e57c23b06af505e21
Agenicy <agenicy@gmail.com>
2022-08-25 18:37:31 +0800

    383 AC(64/31)

---
+++ leetcode/source/383.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace leetcode.Q383
+{
+	public class Program
+	{
+		public static void Run()
+		{
+			Solution solution = new Solution();
+
+			Log.Print(solution.ToString());
+			Log.Print("The answer should be ");
+		}
+	}
+	public class Solution
+	{
+		public bool CanConstruct(string ransomNote, string magazine)
+		{
+			short[] count = new short[26];
+			for (int i = 0; i < magazine.Length; i++)
+			{
+				count[magazine[i] - 'a']++;
+			}
+			for (int i = 0; i < ransomNote.Length; i++)
+			{
+				count[ransomNote[i] - 'a']--;
+				if (count[ransomNote[i] - 'a'] < 0)
+					return false;
+			}
+			return true;
+		}
+	}
+}
