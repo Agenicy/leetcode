5900d59942c83b9cf341b1ea974a569b7545e3b1
Agenicy <agenicy@gmail.com>
2022-09-06 11:34:00 +0800

    814 AC(45/53)

---
+++ leetcode/source/814.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace leetcode.Q814
+{
+	public class Program
+	{
+		public static void Run()
+		{
+			Solution solution = new Solution();
+
+			Log.Print(solution.ToString());
+			Log.Print("The answer should be ");
+		}
+	}
+	public class Solution
+	{
+		public TreeNode PruneTree(TreeNode root)
+		{
+			if (Travsal(root))
+				return root;
+			else
+				return null;
+		}
+
+		bool Travsal(TreeNode parent)
+		{
+			bool left = false, right = false, mid = parent.val == 1;
+			if (!(parent.left is null))
+			{
+				if (!(left = Travsal(parent.left)))
+					parent.left = null;
+			}
+			if (!(parent.right is null))
+			{
+				if (!(right = Travsal(parent.right)))
+					parent.right = null;
+			}
+			return left || right || mid;
+		}
+	}
+}
