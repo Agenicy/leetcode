30e722f7bcde86ffb864d35fa46b46f5c0a47544
Agenicy <agenicy@gmail.com>
2022-09-08 11:46:37 +0800

    94 AC (94/8)

---
+++ leetcode/source/94.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace leetcode.Q94
+{
+	public class Program
+	{
+		public static void Run()
+		{
+			Solution solution = new Solution();
+
+			Log.Print(solution.ToString());
+			Log.Print("The answer should be ");
+		}
+	}
+	public class Solution
+	{
+		public IList<int> InorderTraversal(TreeNode root)
+		{
+			List<int> result = new();
+			void InOrder(TreeNode parent)
+			{
+				if (parent is null)
+					return;
+				InOrder(parent.left);
+				result.Add(parent.val);
+				InOrder(parent.right);
+			}
+			InOrder(root);
+			return result;
+		}
+
+	}
+}
