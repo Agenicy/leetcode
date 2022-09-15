0d70f9963b5925518ed3da36dcba806c0e6380a4
Agenicy <agenicy@gmail.com>
2022-09-15 10:42:07 +0800

    2007 AC (48/44)

--- leetcode/Program.cs
+++ leetcode/Program.cs
@@ -8,7 +8,7 @@ namespace leetcode
 	{
 		static void Main(string[] args)
 		{
-			Q1457.Program.Run();
+			Q2007.Program.Run();
 		}
 	}
 	#region fold
---
+++ leetcode/source/2007.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace leetcode.Q2007
+{
+	public class Program
+	{
+		public static void Run()
+		{
+			Solution solution = new Solution();
+
+			Log.Print(solution.FindOriginalArray(new[] { 0, 0 }));
+			Log.Print("The answer should be ");
+		}
+	}
+	public class Solution
+	{
+		public int[] FindOriginalArray(int[] changed)
+		{
+			Array.Sort(changed);
+			Dictionary<int, int> set = new();
+			foreach (var item in changed)
+			{
+				if (set.ContainsKey(item))
+					set[item]++;
+				else
+					set[item] = 1;
+			}
+
+			List<int> ret = new(changed.Length / 2 + 1);
+
+			for (int i = 0; i < changed.Length; i++)
+			{
+				int f = changed[i];
+				if (set.ContainsKey(f))
+				{
+					if (--set[f] == 0)
+						set.Remove(f);
+					if (set.ContainsKey(f * 2) && set[f * 2] >= 0)
+					{
+						ret.Add(f);
+						if (--set[f * 2] == 0)
+							set.Remove(f * 2);
+					}
+					else return new int[0];
+				}
+			}
+			return ret.ToArray();
+		}
+	}
+}
