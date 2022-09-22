57dfc23753d47987c24fcd9fbe3108dae4688566
Agenicy <Agenicy@Gmail.com>
2022-09-22 13:27:30 +0800

    557 AC (93/7)

---
+++ leetcode/source/557.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcode.Q557
+{
+	public class Program
+	{
+		public static void Run()
+		{
+			Solution solution = new Solution();
+
+			Log.Print(solution.ToString());
+			Log.Print("The answer should be ");
+		}
+	}
+	public class Solution
+	{
+		public string ReverseWords(string s)
+		{
+			return string.Join(' ', s.Split(' ').Select(x => new string(x.Reverse().ToArray())));
+		}
+	}
+}
