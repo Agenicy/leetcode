55ee594a5d5ce8d0e3db2688920710d9c05fb759
Agenicy <agenicy@gmail.com>
2022-10-09 16:14:36 +0800

    653 AC (92/10)

---
+++ leetcode/source/653.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace leetcode.Q
+{
+	public class Program
+	{
+		public static void Run()
+		{
+			Solution solution = new Solution();
+
+			Log.Print(solution.ToString());
+			Log.Print("The answer should be ");
+		}
+	}
+	public class Solution
+	{
+	}
+}
