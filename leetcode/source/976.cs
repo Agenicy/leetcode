fc246a6cc6f4a1cfde462e8efd93711a2c6e9b6b
Agenicy <agenicy@gmail.com>
2022-10-12 16:21:43 +0800

    976 AC (85/43)

---
+++ leetcode/source/976.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace leetcode.Q976
+{
+	public class Program
+	{
+		public static void Run()
+		{
+			Solution solution = new Solution();
+
+			Log.Print(solution.ToString());
+			Log.Print("The answer should be ");
+		}
+	}
+	public class Solution
+	{
+		public int LargestPerimeter(int[] nums)
+		{
+			Array.Sort(nums, (a, b)=>b.CompareTo(a));
+			for (int i = 0; i < nums.Length; i++)
+			{
+				if (i > nums.Length - 2) return 0;
+
+				if (nums[i] < nums[i + 1] + nums[i + 2])
+					return (nums[i] + nums[i + 1] + nums[i + 2]);
+			}
+			return 0;
+		}
+	}
+}
