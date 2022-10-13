7d3f16c42a92dd7e853344200aa49cfc28585a01
Agenicy <Agenicy@Gmail.com>
2022-10-13 13:35:41 +0800

    237 AC (51/55)

---
+++ leetcode/source/237.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace leetcode.Q237
+{
+	public class Program
+	{
+		public static void Run()
+		{
+			Solution solution = new Solution();
+
+			Log.Print(solution.ToString());
+			Log.Print("The answer should be ");
+		}
+	}
+	public class Solution
+	{
+		public void DeleteNode(ListNode node)
+		{
+			node.val = node.next.val;
+			if (node.next.next == null)
+				node.next = null;
+			else
+				DeleteNode(node.next);
+		}
+	}
+}
