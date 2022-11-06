c9b4761f5755f278b9b7367e01ef9a5f1627ca3a
Agenicy <agenicy@gmail.com>
2022-11-06 16:03:34 +0800

    899 AC (14/14)

--- leetcode/Program.cs
+++ leetcode/Program.cs
@@ -8,7 +8,7 @@ namespace leetcode
 	{
 		static void Main(string[] args)
 		{
-			Q212.Program.Run();
+			Q899.Program.Run();
 		}
 	}
 	#region fold
---
+++ leetcode/source/899.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcode.Q899
+{
+	public class Program
+	{
+		public static void Run()
+		{
+			Solution solution = new Solution();
+
+			Log.Print(solution.OrderlyQueue("baaca", 3));
+			Log.Print("The answer should be ");
+		}
+	}
+	public class Solution
+	{
+		public string OrderlyQueue(string s, int k)
+		{
+			LinkedList<char> str = new LinkedList<char>(s);
+			StringBuilder sb = new StringBuilder();
+			while(str.Count > 0)
+			{
+				LinkedListNode<char> min = str.First;
+				LinkedListNode<char> now = str.First.Next;
+				for (int i = 1; i < k & now != null; i++)
+				{
+					if (now.Value < min.Value)
+						min = now;
+					now = now.Next;
+				}
+				sb.Append(min.Value);
+				str.Remove(min);
+			}
+			return sb.ToString();
+		}
+	}
+}
