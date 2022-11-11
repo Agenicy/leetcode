cb4994be624c261b6a3ce9983164dcbba6aa3890
Agenicy <agenicy@gmail.com>
2022-11-11 16:18:09 +0800

    1047 AC (93/7)

---
+++ leetcode/source/1047.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcode.Q1047
+{
+	public class Program
+	{
+		public static void Run()
+		{
+			Solution solution = new Solution();
+
+			Log.Print(solution.ToString());
+			Log.Print("The answer should be ");
+		}
+	}
+	public class Solution
+	{
+		public string RemoveDuplicates(string s)
+		{
+			Stack<char> stack = new();
+			for (int i = 0; i < s.Length; i++)
+			{
+				if (stack.Count > 0 && s[i] == stack.Peek())
+					stack.Pop();
+				else
+					stack.Push(s[i]);
+			}
+			var tmp = stack.Reverse();
+			StringBuilder sb = new();
+			foreach (var item in tmp)
+			{
+				sb.Append(item);
+			}
+			return sb.ToString();
+		}
+	}
+}
