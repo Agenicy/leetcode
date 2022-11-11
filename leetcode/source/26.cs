520c5f82904231b22a5f3fafca1331157eddf240
Agenicy <agenicy@gmail.com>
2022-11-11 16:31:51 +0800

    26 AC (20/50)

---
+++ leetcode/source/26.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcode.Q26
+{
+	public class Program
+	{
+		public static void Run()
+		{
+			Solution solution = new Solution();
+
+			Log.Print(solution.ToString());
+			Log.Print("The answer should be ");
+		}
+	}
+	public class Solution
+	{
+		public int RemoveDuplicates(int[] nums)
+		{
+			int start = 0;
+			for (int i = 1; i < nums.Length; i++)
+			{
+				while (i < nums.Length && nums[start] == nums[i])
+				{ 
+						++i;
+				}
+				if(i < nums.Length)
+					nums[++start] = nums[i];
+			}
+			return start;
+		}
+	}
+}
