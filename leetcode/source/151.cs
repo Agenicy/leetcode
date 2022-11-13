4f66546185b42fbb5c42f0ac5f5995e84b1723db
Agenicy <agenicy@gmail.com>
2022-11-13 10:34:32 +0800

    151 AC (70/58)

---
+++ leetcode/source/151.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcode.Q151
+{
+	public class Program
+	{
+		public static void Run()
+		{
+			Solution solution = new Solution();
+
+			Log.Print(solution.ToString());
+			Log.Print("The answer should be ");
+		}
+	}
+	public class Solution
+	{
+		public string ReverseWords(string s)
+		{
+			return new StringBuilder().AppendJoin(' ', s.Split(' ', StringSplitOptions.RemoveEmptyEntries).Reverse()).ToString();
+		}
+	}
+}
