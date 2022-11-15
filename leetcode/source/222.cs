9974f526d7e717660a7811e51de97b9af2af1ac4
Agenicy <agenicy@gmail.com>
2022-11-15 10:55:18 +0800

    222 AC (85/23)

---
+++ leetcode/source/222.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcode.Q222
+{
+	public class Program
+	{
+		public static void Run()
+		{
+			Solution solution = new Solution();
+
+			Log.Print(solution.ToString());
+			Log.Print("The answer should be ");
+		}
+	}
+	public class Solution
+	{
+		public int CountNodes(TreeNode root)
+		{
+			if (root == null)
+				return 0;
+
+			int count = 1;
+			void DFS(TreeNode parent)
+			{
+				if(parent.left != null)
+				{
+					count++;
+					DFS(parent.left);
+				}
+				if(parent.right != null)
+				{
+					count++;
+					DFS(parent.right);
+				}
+			}
+			DFS(root);
+			return count;
+		}
+	}
+}
