6057363294e9eac55c217073d8aa78fb155b344a
Agenicy <agenicy@gmail.com>
2022-11-18 13:03:13 +0800

    263 AC(86/14)

---
+++ leetcode/source/263.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcode.Q263
+{
+	public class Program
+	{
+		public static void Run()
+		{
+			Solution solution = new Solution();
+
+			Log.Print(solution.ToString());
+			Log.Print("The answer should be ");
+		}
+	}
+	public class Solution
+	{
+		public bool IsUgly(int n)
+		{
+			if (n == 0)
+				return false;
+			foreach (var prime in new[] { 2, 3, 5 })
+				while (n % prime == 0)
+					n /= prime;
+			return n == 1;
+		}
+	}
+}
