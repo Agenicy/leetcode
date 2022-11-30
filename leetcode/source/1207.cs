07445574958d5417bd42f48a60591f604f17a79d
Agenicy <agenicy@gmail.com>
2022-11-30 11:05:39 +0800

    1207 AC (33/67)

---
+++ leetcode/source/1207.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcode.Q1207
+{
+	public class Program
+	{
+		public static void Run()
+		{
+			Solution solution = new Solution();
+
+			Log.Print(solution.ToString());
+			Log.Print("The answer should be ");
+		}
+	}
+	public class Solution
+	{
+		public bool UniqueOccurrences(int[] arr)
+		{
+			Dictionary<int, int> stat = new();
+			foreach (var item in arr)
+			{
+				if (!stat.ContainsKey(item))
+					stat[item] = 1;
+				else
+					stat[item]++;
+			}
+			return new HashSet<int>(stat.Values).Count == stat.Values.Count;
+		}
+	}
+}
