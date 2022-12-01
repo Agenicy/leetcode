0d4b44b46605ab8cc0503d4e8e86010de7ec73b8
Agenicy <agenicy@gmail.com>
2022-12-01 16:59:42 +0800

    1704 AC (77/74)

---
+++ leetcode/source/1704.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcode.Q1704
+{
+	public class Program
+	{
+		public static void Run()
+		{
+			Solution solution = new Solution();
+
+			Log.Print(solution.ToString());
+			Log.Print("The answer should be ");
+		}
+	}
+	public class Solution
+	{
+		public bool HalvesAreAlike(string s)
+		{
+			int count = 0;
+			HashSet<char> vowel = new() { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+			for (int i = 0; i < s.Length/2; i++)
+			{
+				if (vowel.Contains(s[i]))
+				{
+					count++;
+				}
+			}
+			for (int i = s.Length / 2; i < s.Length; i++)
+			{
+				if (vowel.Contains(s[i]))
+				{
+					count--;
+				}
+			}
+			return count == 0;
+		}
+	}
+}
