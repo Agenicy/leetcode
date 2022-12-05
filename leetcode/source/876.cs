016f1557d31af011f26572210aba8ee3596984a2
Agenicy <Agenicy@Gmail.com>
2022-12-05 14:02:13 +0800

    876 AC (87/17)

---
+++ leetcode/source/876.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcode.Q876
+{
+	public class Program
+	{
+		public static void Run()
+		{
+			Solution solution = new Solution();
+
+			Log.Print(solution.ToString());
+			Log.Print("The answer should be ");
+		}
+	}
+	public class Solution
+	{
+		public ListNode MiddleNode(ListNode head)
+		{
+			ListNode next = head;
+			ListNode last = head;
+			while(next?.next != null)
+			{
+				next = next.next?.next;
+				last = last.next;
+			}
+			return last;
+		}
+	}
+}
