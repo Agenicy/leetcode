88bc13d80f36cee24153af83d29508cd64961aa3
Agenicy <agenicy@gmail.com>
2022-12-06 15:05:30 +0800

    328 AC (55/52)

---
+++ leetcode/source/328.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcode.Q328
+{
+	public class Program
+	{
+		public static void Run()
+		{
+			Solution solution = new Solution();
+
+			Log.Print(solution.OddEvenList(null));
+			Log.Print("The answer should be ");
+		}
+	}
+	public class Solution
+	{
+		public ListNode OddEvenList(ListNode head)
+		{
+			if (head?.next == null) return head;
+			ListNode odd, even, oddHead, evenHead;
+			oddHead = odd = head;
+			evenHead = even = head.next;
+			for (int i = 0; odd.next != null & even.next != null; i++)
+			{
+				if(i%2==0)
+				{
+					odd = odd.next = even.next;
+				}
+				else
+				{
+					even = even.next = odd.next;
+				}
+			}
+			odd.next = evenHead;
+			even.next = null;
+			return oddHead;
+		}
+	}
+}
