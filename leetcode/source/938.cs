97b8256d8015c112421c28adf82e263f3a5e1714
Agenicy <agenicy@gmail.com>
2022-12-07 11:26:48 +0800

    938 AC(100/8)

---
+++ leetcode/source/938.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace leetcode.Q938
+{
+	public class Program
+	{
+		public static void Run()
+		{
+			Solution solution = new Solution();
+
+			Log.Print(solution.ToString());
+			Log.Print("The answer should be ");
+		}
+	}
+	public class Solution
+	{
+		public int RangeSumBST(TreeNode root, int low, int high)
+		{
+			int count = 0;
+			void Add(TreeNode parent)
+			{
+				if (parent == null) { return; }
+				if (low <= parent.val & parent.val <= high)
+					count += parent.val;
+				Add(parent.left);
+				Add(parent.right);
+			}
+			Add(root);
+			return count;
+		}
+	}
+}
