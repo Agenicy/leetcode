44f1f035c5b489873196b520a2abea8071020063
Agenicy <agenicy@gmail.com>
2022-12-09 11:10:32 +0800

    1026 AC (87/9)

--- leetcode/Program.cs
+++ leetcode/Program.cs
@@ -8,7 +8,7 @@ namespace leetcode
 	{
 		static void Main(string[] args)
 		{
-			Q872.Program.Run();
+			Q1026.Program.Run();
 		}
 	}
 	#region fold
---
+++ leetcode/source/1026.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcode.Q1026
+{
+	public class Program
+	{
+		public static void Run()
+		{
+			Solution solution = new Solution();
+
+			Log.Print(solution.MaxAncestorDiff(TreeNode.Build("[2,0,8,3,10,7,1,4,null,null,null,null,null,null,6,5,null,9]")));
+			Log.Print("The answer should be ");
+		}
+	}
+	public class Solution
+	{
+		public int MaxAncestorDiff(TreeNode root)
+		{
+			(int, int) Count(TreeNode parent, ref int maxDiff)
+			{
+				if ((parent.left == null) & (parent.right == null))
+				{
+					return (parent.val, parent.val);
+				}
+				int min = int.MaxValue;
+				int max = int.MinValue;
+				if (parent.left != null)
+				{
+					(min, max) = Count(parent.left, ref maxDiff);
+				}
+				if (parent.right != null)
+				{
+					(int m, int M) = Count(parent.right, ref maxDiff);
+					min = Math.Min(min, m);
+					max = Math.Max(max, M);
+				}
+
+				if (parent.val < min)
+					min = parent.val;
+				else
+					maxDiff = Math.Max(maxDiff, Math.Abs(parent.val - min));
+				if (parent.val > max)					max = parent.val;
+				else
+					maxDiff = Math.Max(maxDiff, Math.Abs(parent.val - max));
+				return (min, max);
+			}
+			int MD = 0;
+			Count(root, ref MD);
+			return MD;
+		}
+	}
+}
