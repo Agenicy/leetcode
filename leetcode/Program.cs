727da6daa3477e050a5f7ee1dc9c16b1bc4c4b8b
Agenicy <agenicy@gmail.com>
2022-12-14 14:40:44 +0800

    198 AC (94/12)

--- leetcode/Program.cs
+++ leetcode/Program.cs
@@ -8,7 +8,7 @@ namespace leetcode
 	{
 		static void Main(string[] args)
 		{
-			Q124.Program.Run();
+			Q198.Program.Run();
 		}
 	}
 	#region fold
---
+++ leetcode/source/198.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcode.Q198
+{
+	public class Program
+	{
+		public static void Run()
+		{
+			Solution solution = new Solution();
+
+			Log.Print(solution.Rob(Parser.ParseArr1D("[2,7,9,3,1]")));
+			Log.Print("The answer should be ");
+		}
+	}
+	public class Solution
+	{
+		public int Rob(int[] nums)
+		{
+			if (nums.Length < 2)
+				return nums.Max();
+
+			nums[2] += nums[0];
+			for (int i = 3; i < nums.Length; i++)
+			{
+				nums[i] += Math.Max(nums[i - 3], nums[i - 2]);
+			}
+			return Math.Max(nums[nums.Length - 2], nums[nums.Length - 1]);
+		}
+	}
+}
