f3f9478c6ac044b31831e67369ec562913f66312
Agenicy <agenicy@gmail.com>
2022-12-16 10:35:14 +0800

    232 AC (89/15)

---
+++ leetcode/source/232.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcode.Q232
+{
+	public class Program
+	{
+		public static void Run()
+		{
+			Log.Print("The answer should be ");
+		}
+	}
+	public class MyQueue
+	{
+		Queue<int> inQ, outQ;
+		public MyQueue()
+		{
+			inQ = new();
+			outQ = new();
+		}
+
+		public void Push(int x)
+		{
+			while (outQ.Count > 0)
+				inQ.Enqueue(outQ.Dequeue());
+
+			inQ.Enqueue(x);
+		}
+
+		public int Pop()
+		{
+			while (inQ.Count > 0)
+				outQ.Enqueue(inQ.Dequeue());
+
+			return outQ.Dequeue();
+		}
+
+		public int Peek()
+		{
+			while (inQ.Count > 0)
+				outQ.Enqueue(inQ.Dequeue());
+
+			return outQ.Peek();
+		}
+
+		public bool Empty()
+		{
+			return inQ.Count == 0 & outQ.Count == 0;
+		}
+	}
+
+}
