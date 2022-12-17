299e51916f648b1fc1ec3eb6777e897f09a9b679
Agenicy <agenicy@gmail.com>
2022-12-18 01:10:32 +0800

    150 AC (85/19)

--- leetcode/Program.cs
+++ leetcode/Program.cs
@@ -8,7 +8,7 @@ namespace leetcode
 	{
 		static void Main(string[] args)
 		{
-			Q198.Program.Run();
+			Q150.Program.Run();
 		}
 	}
 	#region fold
---
+++ leetcode/source/150.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcode.Q150
+{
+	public class Program
+	{
+		public static void Run()
+		{
+			Solution solution = new Solution();
+
+			Log.Print(solution.EvalRPN(new[] { "4", "13", "5", "/", "+" }));
+			Log.Print("The answer should be ");
+		}
+	}
+	public class Solution
+	{
+		public int EvalRPN(string[] tokens)
+		{
+			Stack<int> stack = new();
+			int a, b;
+			foreach (var next in tokens)
+			{
+				switch (next)
+				{
+					case "+":
+						a = stack.Pop(); b = stack.Pop();
+						stack.Push(b + a);
+						break;
+					case "-":
+						a = stack.Pop(); b = stack.Pop();
+						stack.Push(b - a);
+						break;
+					case "*":
+						a = stack.Pop(); b = stack.Pop();
+						stack.Push(b * a);
+						break;
+					case "/":
+						a = stack.Pop(); b = stack.Pop();
+						stack.Push(b / a);
+						break;
+					default:
+						stack.Push(int.Parse(next));
+						break;
+				}
+			}
+			return stack.Pop();
+		}
+	}
+}
