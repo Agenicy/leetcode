e2813882f10ad6f8670517df4d2d1c9d4f4fac6c
Agenicy <agenicy@gmail.com>
2023-01-03 17:37:26 +0800

    944 AC (69/5)

---
+++ leetcode/source/944.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcode.Q944
+{
+	public class Program
+	{
+		public static void Run()
+		{
+			Solution solution = new Solution();
+
+			Log.Print(solution.ToString());
+			Log.Print("The answer should be ");
+		}
+	}
+	public class Solution
+	{
+		public int MinDeletionSize(string[] strs)
+		{
+			int[] order = new int[strs[0].Length];
+			int sum = 0;
+			for (int j = 0; j < strs.Length; j++)
+			{
+				for (int i = 0; i < strs[0].Length; i++)
+				{
+					if (order[i] > 0)
+					{
+						if (strs[j][i] - 'a' < order[i])
+						{
+							sum++;
+							order[i] = -1;
+						}
+						else
+							order[i] = strs[j][i] - 'a';
+					}
+				}
+			}
+			return sum;
+		}
+	}
+}
