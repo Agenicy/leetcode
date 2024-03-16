c0a442a14d2185add391a9be65721477a7b8f4f8
Agenicy <agenicy@gmail.com>
2024-03-16 16:18:09 +0800

    238+

--- leetcode/source/238.cs
+++ leetcode/source/238.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
+using System.Reflection;
 using System.Text;
 
 namespace leetcode.Q238
@@ -11,12 +12,11 @@ namespace leetcode.Q238
 		public static void Run()
 		{
 			Solution solution = new Solution();
-			//Log.Print(solution.ProductExceptSelf(new int[] { 1, 2, 3, 4 }));
+			Log.Print(solution.ProductExceptSelf(new int[] { 1, 2, 3, 4 }));
 			Log.Print("The answer should be [24,12,8,6]");
-			//Log.Print(solution.ProductExceptSelf(new int[] { -1, 1, 0, -3, 3 }));
+			Log.Print(solution.ProductExceptSelf(new int[] { -1, 1, 0, -3, 3 }));
 			Log.Print("The answer should be [0,0,9,0,0] ");
-			//Log.Print(solution.ProductExceptSelf(new int[] { 2, 2, 0, 2, 2, 0, 0, 2, 2 }));
-			Log.Print(solution.ProductExceptSelf(Enumerable.Repeat(1, 50000).ToArray()));
+			Log.Print(solution.ProductExceptSelf(new int[] { 2, 2, 0, 2, 2, 0, 0, 2, 2 }));
 		}
 	}
 	public class Solution
@@ -31,18 +31,26 @@ namespace leetcode.Q238
 				return nums;
 			}
 
-			int level = BitOperations.Log2((uint)nums.Length) + 1;
-			int length = 1 << level;
+			int length = 1;
+			while (length < nums.Length)
+			{
+				length = length << 1;
+			}
 
-			int[] heap = new int[length * 2];
+			int[] heap = new int[length];
+			Array.Fill(heap, 1);
 
 			// init heap
-			for (int i = length - 1; i >= 0; i--)
+			
+			for (int i = 0; i < nums.Length; i++)
 			{
-				heap[i + length] = i >= nums.Length ? 1 : nums[i];
+				// level 1
+				heap[(i + length) / 2] *= nums[i];
 			}
-			for (int i = length - 1; i > 1; i--)
+
+			for (int i = length / 2 - 1; i > 1; i--)
 			{
+				// level 2+
 				heap[i] = heap[i * 2] * heap[i * 2 + 1];
 			}
 
@@ -51,12 +59,16 @@ namespace leetcode.Q238
 				heap[i ^ 1] *= heap[(i / 2) ^ 1];
 			}
 
-			for (int i = 0; i < nums.Length; i++)
+			for (int i = 0; i < nums.Length; i += 2)
 			{
-				nums[i] = heap[(i + length) ^ 1] * heap[(i + length) / 2 ^ 1];
+				int temp = 1;
+				if (i + 1 < nums.Length)
+				{
+					temp = nums[i + 1];
+					nums[i + 1] = nums[i] * heap[(i + 1 + length) / 2 ^ 1];
+				}
+				nums[i] = temp * heap[(i + length) / 2 ^ 1];
 			}
-
-
 			return nums;
 		}
 	}
